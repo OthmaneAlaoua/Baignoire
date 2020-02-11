d3e4f42ea8d596f4893434c59710180e63ea29af
alaoua_o <alaoua_o@etna-alternance.net>
2020-02-11 17:05:16 +0100

    ajout step 2

---
+++ Step 2/AbstractEntity.hh
@@ -0,0 +1,22 @@
+//
+// Created by Othmane ALAOUA on 06/02/2020.
+//
+
+#ifndef STEP1_ABSTRACTENTITY_HH
+#define STEP1_ABSTRACTENTITY_HH
+
+
+class AbstractEntity {
+private:
+    virtual void Draw() = 0;
+
+    virtual void Update() = 0;
+
+public:
+    AbstractEntity();
+
+    virtual ~AbstractEntity();
+
+};
+
+#endif //STEP1_ABSTRACTENTITY_HH
